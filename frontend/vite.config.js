51eeff5d7cc7481b863c776cb76503b4f6ea4689
Lofienjoyer <dan-kol@bk.ru>
2026-04-08 22:08:43 +0300

    chore(front): update vite.config.js server config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,4 +15,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: true, // 👈 ВАЖНО
+    port: 5173  // (опционально)
+  }
 })
