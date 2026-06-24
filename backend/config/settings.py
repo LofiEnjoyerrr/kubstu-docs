d6f980e4e0ea46b8775b5426ff6d2a120c6ff8f1
Lofienjoyer <dan-kol@bk.ru>
2026-06-24 16:15:32 +0300

    feat

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -6,7 +6,7 @@ from environ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-environ.Env.read_env(BASE_DIR.parent / '.env')
+environ.Env.read_env(BASE_DIR / '.env')
 
 SECRET_KEY = env('SECRET_KEY')
 
